165af505dde1f2dfc18b05b009414d6f3b802313
alzaw <alzaw@10.0.0.86>
2020-03-18 19:06:38 -0400

    initial import of project

---
+++ src/introduction/HelloJava.java
@@ -0,0 +1,11 @@
+package introduction;
+
+//A simple introductory Java class. 
+
+public class HelloJava {
+	
+	public static void main(String[] args) { 
+		
+		System.out.print("Alzawad Takrim");
+}
+}
